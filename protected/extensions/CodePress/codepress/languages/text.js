9d48f696ded739c84e188444d994609d0e7b1394
mburtscher <>
2009-03-26 15:23:44 +0000

    * Added CodePress extension

---
+++ protected/extensions/CodePress/codepress/languages/text.js
@@ -0,0 +1,11 @@
+/*
+ * CodePress regular expressions for Text syntax highlighting
+ */
+
+var Language = new Object();
+
+// plain text
+Language.syntax = []
+Language.snippets = []
+Language.complete = []
+Language.shortcuts = []
