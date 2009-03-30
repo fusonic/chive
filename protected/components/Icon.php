a23b495dc1de4409e1df05dc402006a637d37418
mburtscher <>
2009-03-30 08:28:09 +0000

    * Icon widget * Iconpack in config

---
+++ protected/components/Icon.php
@@ -0,0 +1,30 @@
+<?php
+
+class Icon extends CWidget
+{
+
+	public $htmlOptions;
+	public $size = 16;
+	public $name;
+	public $text = "core.unknown";
+	public $disabled = false;
+
+	public function run() {
+
+		list($category, $var) = explode(".", $this->text);
+
+		$classes = "icon icon" . $this->size . ($this->disabled ? " disabled" : "");
+		if(isset($this->htmlOptions['class']))
+		{
+			$this->htmlOptions['class'] .= " " . $classes;
+		}
+		else
+		{
+			$this->htmlOptions['class'] = $classes;
+		}
+
+		echo CHtml::image(Yii::app()->params->iconpack . DIRECTORY_SEPARATOR . $this->size . DIRECTORY_SEPARATOR . $this->name . ".png", Yii::t($category, $var), $this->htmlOptions);
+
+	}
+
+}
--- protected/config/main.php
+++ protected/config/main.php
@@ -97,6 +97,7 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
+		'iconpack'=>'images/icons/pma',
 	),
 
 	'sourceLanguage'=>'asdf',
