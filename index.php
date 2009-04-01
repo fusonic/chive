6fa8f2b751d6c8a38c56c4f442eebe48cc963081
rponudic <>
2009-04-01 13:51:56 +0000

    *Oops2

--- index.php
+++ index.php
@@ -12,20 +12,20 @@ function pre($_value) {
 
 function predie($_value) {
 	pre($_value);
-	Yii::app()->end();
+	CApplication::end();
 }
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
-require_once($yii);
 
-$console = true;
+$console = false;
 
-if($console == true) {
+if($console == true)
 	$config = dirname(__FILE__).'/protected/config/dev.php';
-}
 
+require_once($yii);
 $app = Yii::createWebApplication($config);
+
 
 if(!$app->user->isGuest) {
 	$app->db->connectionString = 'mysql:host=' . $app->user->host . ';dbname=information_schema';
