96cc3499aeee8e0f23becd9e586394327d24ff72
mburtscher <>
2009-05-11 09:34:40 +0000

    * Shrinked test for SqlQuery * Added Index test

---
+++ tests/models/Index.php
@@ -0,0 +1,236 @@
+<?php
+
+class IndexTest extends TestCase
+{
+
+	/**
+	 * Setup test databases.
+	 */
+	protected function setUp()
+	{
+		$this->executeSqlFile('models/Index.sql');
+		$db = new CDbConnection('mysql:host='.DB_HOST.';dbname=indextest', DB_USER, DB_PASSWORD);
+		$db->charset='utf8';
+		$db->active = true;
+		Table::$db = $db;
+		Index::$db = $db;
+	}
+
+	/**
+	 * Loads indices of test table.
+	 */
+	public function testLoad()
+	{
+		// Load table
+		$table = Table::model()->findByPk(array(
+			'TABLE_SCHEMA' => 'indextest',
+			'TABLE_NAME' => 'table1',
+		));
+
+		// Check index count
+		$this->assertEquals(4, count($table->indices));
+
+		// Check index 1
+		$index = $table->indices[0];
+		$this->assertEquals($table->TABLE_NAME, $index->table->TABLE_NAME);
+		$this->assertEquals('PRIMARY', $index->INDEX_NAME);
+		$this->assertEquals('PRIMARY', $index->getType());
+
+		// Check index 2
+		$index = $table->indices[1];
+		$this->assertEquals('unique', $index->INDEX_NAME);
+		$this->assertEquals('UNIQUE', $index->getType());
+
+		// Check index 2
+		$index = $table->indices[2];
+		$this->assertEquals('index', $index->INDEX_NAME);
+		$this->assertEquals('INDEX', $index->getType());
+
+		// Check index 2
+		$index = $table->indices[3];
+		$this->assertEquals('fulltext', $index->INDEX_NAME);
+		$this->assertEquals('FULLTEXT', $index->getType());
+	}
+
+	/**
+	 * Checks deletion of indices.
+	 */
+	public function testDelete()
+	{
+		// Load table
+		$table = Table::model()->findByPk(array(
+			'TABLE_SCHEMA' => 'indextest',
+			'TABLE_NAME' => 'table2',
+		));
+
+		// Delete all indices
+		foreach($table->indices AS $index)
+		{
+			$this->assertNotSame(false, $index->delete());
+		}
+
+		// Reload table
+		$table->refresh();
+
+		// Check index count
+		$this->assertEquals(0, count($table->indices));
+	}
+
+	/**
+	 * Checks what happens when deleting auto_increment primary key.
+	 *
+	 * @expectedException DbException
+	 */
+	public function testDeleteAutoIncrement()
+	{
+		// Load table
+		$table = Table::model()->findByPk(array(
+			'TABLE_SCHEMA' => 'indextest',
+			'TABLE_NAME' => 'table1',
+		));
+
+		// Delete key
+		$res = $table->indices[0]->delete();
+
+		// Check result
+		$this->assertEquals(false, $res);
+
+		// Set throwExceptions and try again
+		$table->indices[0]->throwExceptions = true;
+		$table->indices[0]->delete();
+	}
+
+	/**
+	 * Test update type/name/columns.
+	 */
+	public function testUpdate()
+	{
+		// Load index
+		$index = Index::model()->findByAttributes(array(
+			'INDEX_NAME' => 'index',
+			'TABLE_NAME' => 'table2',
+			'TABLE_SCHEMA' => 'indextest',
+		));
+		$index->throwExceptions = true;
+
+		// Change properties
+		$index->INDEX_NAME = 'newname';
+		$index->setType('UNIQUE');
+
+		// Add new column
+		$columns = $index->columns;
+		$col = new IndexColumn();
+		$col->COLUMN_NAME = 'pk';
+		$columns[] = $col;
+		$col = new IndexColumn();
+		$col->COLUMN_NAME = 'varchar';
+		$col->SUB_PART = 10;
+		$columns[] = $col;
+		$index->columns = $columns;
+
+		// Try saving
+		$index->save();
+
+		// Reload index and load index columns
+		$index->refresh();
+		$cols = IndexColumn::model()->findAllByAttributes(array('TABLE_SCHEMA' => $index->TABLE_SCHEMA, 'TABLE_NAME' => $index->TABLE_NAME, 'INDEX_NAME' => $index->INDEX_NAME));
+
+		// Check properties
+		$this->assertEquals(2, count($cols));
+		$this->assertEquals('newname', $index->INDEX_NAME);
+		$this->assertEquals('UNIQUE', $index->getType());
+		$this->assertNull($cols[0]->SUB_PART);
+		$this->assertEquals(10, $cols[1]->SUB_PART);
+	}
+
+	/**
+	 * Tests to update a new record
+	 *
+	 * @expectedException CException
+	 */
+	public function testUpdateNew()
+	{
+		$index = new Index();
+		$index->update();
+	}
+
+	/**
+	 * Tests to delete a new record
+	 *
+	 * @expectedException CException
+	 */
+	public function testDeleteNew()
+	{
+		$index = new Index();
+		$index->delete();
+	}
+
+	/**
+	 * Tests to insert a existing record.
+	 *
+	 * @expectedException CException
+	 */
+	public function testInsertExisting()
+	{
+		$index = Index::model()->findByAttributes(array(
+			'INDEX_NAME' => 'index',
+			'TABLE_NAME' => 'table2',
+			'TABLE_SCHEMA' => 'indextest',
+		));
+		$index->insert();
+	}
+
+	public function testCreate()
+	{
+		// Create index
+		$index = new Index();
+		$index->TABLE_NAME = 'table2';
+		$index->TABLE_SCHEMA = 'indextest';
+		$index->INDEX_NAME = 'newname';
+		$index->setType('UNIQUE');
+
+		// Add new column
+		$columns = $index->columns;
+		$col = new IndexColumn();
+		$col->COLUMN_NAME = 'pk';
+		$columns[] = $col;
+		$col = new IndexColumn();
+		$col->COLUMN_NAME = 'varchar';
+		$col->SUB_PART = 10;
+		$columns[] = $col;
+		$index->columns = $columns;
+
+		// Try saving
+		$index->save();
+
+		// Reload index and load index columns
+		$index->refresh();
+		$cols = IndexColumn::model()->findAllByAttributes(array('TABLE_SCHEMA' => $index->TABLE_SCHEMA, 'TABLE_NAME' => $index->TABLE_NAME, 'INDEX_NAME' => $index->INDEX_NAME));
+
+		// Check properties
+		$this->assertEquals(2, count($cols));
+		$this->assertEquals('newname', $index->INDEX_NAME);
+		$this->assertEquals('UNIQUE', $index->getType());
+		$this->assertNull($cols[0]->SUB_PART);
+		$this->assertEquals(10, $cols[1]->SUB_PART);
+	}
+
+	/**
+	 * Tests some config functions.
+	 */
+	public function testConfigFunctions()
+	{
+		// Create new schema
+		$index = new Index();
+
+		// Check return types
+		$this->assertTrue(is_array($index->safeAttributes()));
+		$this->assertTrue(is_array($index->attributeLabels()));
+		$this->assertTrue(is_array($index->rules()));
+		$this->assertTrue(is_array($index->relations()));
+		$this->assertTrue(is_array(Index::getIndexTypes()));
+	}
+
+}
+
+?>
---
+++ tests/models/Index.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS `indextest`;
+CREATE DATABASE `indextest` DEFAULT COLLATE 'utf8_general_ci';
+
+DROP TABLE IF EXISTS `indextest`.`table1`;
+CREATE TABLE IF NOT EXISTS `indextest`.`table1` (
+  `pk` int(11) NOT NULL auto_increment,
+  `varchar` varchar(50) NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY  (`pk`),
+  UNIQUE KEY `unique` (`datetime`),
+  KEY `index` (`varchar`),
+  FULLTEXT KEY `fulltext` (`varchar`)
+);
+
+DROP TABLE IF EXISTS `indextest`.`table2`;
+CREATE TABLE IF NOT EXISTS `indextest`.`table2` (
+  `pk` int(11) NOT NULL,
+  `varchar` varchar(50) NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY  (`pk`),
+  UNIQUE KEY `unique` (`datetime`),
+  KEY `index` (`varchar`),
+  FULLTEXT KEY `fulltext` (`varchar`)
+);
