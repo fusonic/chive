ceb62e7a9b6dc64bbc6ca1c27d5479c6224961f6
dmoesslang <>
2009-06-16 11:49:12 +0000

    * new tests added (Characterset model)

---
+++ tests/models/CharacterSet.php
@@ -0,0 +1,31 @@
+<?php
+
+class CharacterSetTest extends TestCase
+{
+	/**
+	 * tests the characterset model
+	 */
+	public function testModel()
+	{
+		$char = CharacterSet::model()->findAll();
+
+		$this->assertType('array',$char);
+
+	}
+
+	/**
+	 * tests some Conifg
+	 */
+	public function testConfig()
+	{
+		$char = new CharacterSet();
+
+		$this->assertType('string',$char->tableName());
+
+		$this->assertType('string',$char->primaryKey());
+
+		$this->assertType('array',$char->relations());
+
+	}
+
+}
