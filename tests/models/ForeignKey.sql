d628bae6b08aed18a669825f54f39041c8ec9fe5
dmoesslang <>
2009-06-16 14:08:02 +0000

    *test sql updated(foreignkey model)

--- tests/models/ForeignKey.sql
+++ tests/models/ForeignKey.sql
@@ -7,7 +7,6 @@ DROP TABLE IF EXISTS `tabletest`.`customer`;
 CREATE TABLE `tabletest`.`customer` (
 id INT NOT NULL, 
 test INT NOT NULL,
-test2 VARCHAR,
 PRIMARY KEY (id))
 ENGINE=INNODB;
 
@@ -31,6 +30,5 @@ CREATE TABLE `tabletest`.`product`(
 category INT NOT NULL,
 id INT NOT NULL,
 fk INT NOT NULL,
-test3 BOOL,
 price DECIMAL,
 PRIMARY KEY(category, id)) ENGINE=INNODB;
