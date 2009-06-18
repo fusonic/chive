36f7cc877159cd83826e4eb06acca38208e4e38e
dmoesslang <>
2009-06-18 14:28:37 +0000

    *new tests added (routine model)

--- tests/models/Routine.php
+++ tests/models/Routine.php
@@ -4,9 +4,9 @@ class RoutineTest extends TestCase
 {
 	protected function setUp()
 	{
-		$this->executeSqlFile('models/Schema.sql');
-		Routine::$db = new CDbConnection('mysql:host='.DB_HOST.';dbname=information_schema', DB_USER, DB_PASSWORD);
-	
+		$this->executeSqlFile('models/Routine.sql');
+		Routine::$db = new CDbConnection('mysql:host='.DB_HOST.';dbname=routinetest', DB_USER, DB_PASSWORD);
+		Routine::$db->active = true;
 	}
 
 	public function testModel()
@@ -17,15 +17,33 @@ class RoutineTest extends TestCase
 
 	public function testConfig()
 	{
-		$this->assertType('array',Routine::primaryKey());
-		$this->assertType('string',Routine::tableName());
+		$routin = new Routine();
+		
+		$this->assertType('array',$routin->primaryKey());
+		$this->assertType('string',$routin->tableName());
 	}
-	
-	
+
+
 	public function testDelete()
 	{
-		
-		
+		$routineObj = Routine::model()->findByPk(array(
+				'ROUTINE_SCHEMA' => 'routinetest',
+				'ROUTINE_NAME' => 'test_procedure',
+		));
+
+		$this->assertType('string',$routineObj->delete());
+			
+	}
+
+	public function testGetRoutine()
+	{
+		$routineObj = Routine::model()->findByPk(array(
+				'ROUTINE_SCHEMA' => 'routinetest',
+				'ROUTINE_NAME' => 'test_procedure',
+		));
+
+		$this->assertType('string',$routineObj->getCreateRoutine());
+			
 	}
 }
 
---
+++ tests/models/Routine.sql
@@ -0,0 +1,22 @@
+
+DROP DATABASE IF EXISTS `routinetest`;
+CREATE DATABASE `routinetest` DEFAULT COLLATE 'utf8_general_ci';
+
+DROP TABLE IF EXISTS `routinetest`.`table1`;
+CREATE TABLE IF NOT EXISTS `routinetest`.`table1` (
+  `pk` int(11) NOT NULL auto_increment,
+  `varchar` varchar(50) NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY  (`pk`),
+  UNIQUE KEY `unique` (`datetime`),
+  KEY `index` (`varchar`),
+  FULLTEXT KEY `fulltext` (`varchar`)
+);
+
+DROP PROCEDURE IF EXISTS `routinetest`.`test_procedure`;
+
+CREATE PROCEDURE `routinetest`.`test_procedure`()
+BEGIN
+
+
+END
