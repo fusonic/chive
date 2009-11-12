9e813746d41929eacb54aff72c222beea9296c55
Matthias Burtscher <matthias.burtscher@fusonic.net>
2009-11-12 09:15:56 +0100

    * Broken main.js after licence text changes

--- js/main.js
+++ js/main.js
@@ -17,6 +17,205 @@
  * You should have received a copy of the GNU General Public
  * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
+
+var editing = false;
+var globalPost = {};
+
+function init() 
+{
+	$('table.list').each(function() {
+		var tBody = this.tBodies[0];
+		var rowCount = tBody.rows.length;
+		var currentClass = 'odd';
+		for(var i = 0; i < rowCount; i++)
+		{
+			if(!tBody.rows[i].className.match('noSwitch') || i == 0)
+			{
+				if(currentClass == 'even')
+				{
+					currentClass = 'odd';
+				}
+				else
+				{
+					currentClass = 'even';
+				}
+			}
+			tBody.rows[i].className += ' ' + currentClass;
+		}
+	});
+	
+	// Add checkboxes to respective tables
+	try 
+	{
+		$('table.addCheckboxes').addCheckboxes().removeClass('addCheckboxes');
+		$('table.editable').editableTable().removeClass('editable');
+	}
+	catch(ex)
+	{
+	}
+	
+	// Reset favicon
+	/*
+	$('link[rel="shortcut icon"]').attr('href', baseUrl + '/images/favicon2.ico');
+	window.setTimeout(function() { $('link[rel="shortcut icon"]').attr('href', baseUrl + '/images/favicon.ico'); }, 3000);
+	*/
+	// Unset editing
+	editing = false;
+}
+
+function navigateTo(_url, _post)
+{
+	globalPost = _post;
+	window.location.href = _url;
+	
+	return false;
+}
+
+$(document).ready(function()
+{
+	// Load sideBar
+	var sideBar = $("#sideBar");
+	
+	$('body').layout({
+		
+		// General
+		applyDefaultStyles: true,
+
+		// North
+		north__size: 40,
+		north__resizable: false,
+		north__closable: false,
+		north__spacing_open: 1,
+
+		// West
+		west__size: userSettings.sidebarWidth,
+		west__initClosed: userSettings.sidebarState == 'closed',
+		west__onresize_end: function () {
+			sideBar.accordion('resize');
+			if($('.ui-layout-west').width() != userSettings.sidebarWidth)
+			{
+				// Save
+				userSettings.sidebarWidth = $('.ui-layout-west').width(); 
+				$.post(baseUrl + '/ajaxSettings/set', {
+						name: 'sidebarWidth',
+						value: $('.ui-layout-west').width()
+					}
+				);
+			}
+			return;
+		},
+		west__onclose_end: function () {
+			sideBar.accordion('resize');
+			// Save
+			$.post(baseUrl + '/ajaxSettings/set', {
+					name: 'sidebarState',
+					value: 'closed'
+				}
+			);
+			return;
+		},
+		west__onopen_end: function () {
+			sideBar.accordion('resize');
+			// Save
+			$.post(baseUrl + '/ajaxSettings/set', {
+					name: 'sidebarState',
+					value: 'open'
+				}
+			);
+			return;
+		}
+	});
+	
+
+	// ACCORDION - inside the West pane
+	sideBar.accordion({
+		animated: "slide",
+		addClasses: false,
+		autoHeight: true,
+		collapsible: false,
+		fillSpace: true,
+		selectedClass: "active"
+	});
+	
+	// Trigger resize event for sidebar accordion - doesn't work in webkit-based browsers
+	sideBar.accordion('resize');
+	
+
+	// Setup list filters
+
+	$('#schemaList').setupListFilter($('#schemaSearch'));
+	$('#tableList').setupListFilter($('#tableSearch'));
+	$('#viewList').setupListFilter($('#viewSearch'));
+	$('#bookmarkList').setupListFilter($('#bookmarkSearch'));
+	
+
+	
+	/*
+	 * Ajax functions
+	 */ 
+	
+	// START
+	$(document).ajaxStart(function() {
+		$('#loading').css({'background-image': 'url(' + baseUrl + '/images/loading4.gif)'}).fadeIn();
+		//$('#loading2').show();
+	});
+	
+	// STOP
+	$(document).ajaxStop(function() {
+		//$('#loading2').hide();
+		$('#loading').css({'background-image': 'url(' + baseUrl + '/images/loading5.gif)'}).fadeOut();
+	});
+	
+	// ERROR
+	$(document).ajaxError(function() {
+		Notification.add('warning', 'Ajax request failed', 'Click <a href="javascript:void(0);" onclick="reload();">here</a> to reload site.', null);
+		$('#loading').css({'background-image': 'url(' + baseUrl + '/images/loading5.gif)'}).fadeOut();
+	});
+
+	/*
+	 * Change jQuery UI dialog defaults
+	 */
+	$.ui.dialog.defaults.autoOpen = false;
+	$.ui.dialog.defaults.modal = true;
+	$.ui.dialog.defaults.resizable = false;
+
+
+	/*
+	 * Misc
+	 */
+	chive.init();
+	
+})
+.keydown(function(e) 
+{
+	if(e.keyCode >= 48 
+		&& e.keyCode <= 90
+		&& !e.altKey && !e.ctrlKey && !e.shiftKey 
+		&& (e.target == null || (e.target.tagName != 'INPUT' && e.target.tagName != 'TEXTAREA' && e.target.tagName != 'SELECT')))
+	{
+		var element = $('#tableSearch:visible, #schemaSearch:visible');
+		if(element.length == 1)
+		{
+			element = element.get(0);
+			element.value = '';
+			element.focus();
+		}
+	}
+});
+
+String.prototype.trim = function() {
+    return this.replace(/^\s*/, "").replace(/\s*$/, "");
+}
+
+String.prototype.startsWith = function(str)
+{
+	return (this.match("^"+str)==str);
+}
+
+
+/*
+ * Language
+ */
 var lang = {
 	
 	get: function(category, variable, parameters) 
