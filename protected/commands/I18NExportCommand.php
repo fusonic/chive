59c98ba55ad20e6caa2afbc4c6dc05ca1e768802
Matthias Burtscher <matthias@matthias-laptop>
2010-02-24 22:39:06 +0100

    * Fixed Yii Console (wrong paths were set) * Added I18NExportCommand to console * Added first export of chive.pot (translation template for launchpad)

---
+++ protected/commands/I18NExportCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+class I18NExportCommand extends CConsoleCommand
+{
+	
+	/**
+	 * @see		CConsoleCommand::run()
+	 */
+	public function run($args)
+	{
+		// Find all source files (english)
+		$files = array(
+			'core',
+			'dataTypes',
+		);
+		
+		// Create an empty array for all texts
+		$strings = array();
+		
+		// Go through all files
+		foreach($files as $file)
+		{
+			// Load XML
+			$xml = new SimpleXMLElement(file_get_contents('messages/en/' . $file . '.xml'));
+			
+			foreach($xml->entry as $entry)
+			{
+				$strings[] = (string)$entry;
+			}
+		}
+		
+		// Open target file to write
+		$fp = fopen("messages/_translation/chive.pot", "w");
+		
+		// Write all strings
+		foreach(array_unique($strings) as $string)
+		{
+			fputs($fp, 'msgid "' . $string . '"' . "\n");
+			fputs($fp, 'msgstr ""' . "\n");
+		}
+		
+		fclose($fp);
+	}
+	
+}
--- protected/yiic.php
+++ protected/yiic.php
@@ -22,7 +22,7 @@
 
 
 // change the following paths if necessary
-$yiic='/var/www/testing/php/framework/yii/yii/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
+$yiic = dirname(__FILE__) . '/../yii/yiic.php';
+$config = dirname(__FILE__) . '/config/console.php';
 
 require_once($yiic);
