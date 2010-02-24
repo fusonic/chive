e259105352fc0924ef56a3772893d33fc11f481e
Matthias Burtscher <matthias@matthias-laptop>
2010-02-24 23:05:00 +0100

    * Updated I18NExportCommand * Updated chive.pot

--- protected/commands/I18NExportCommand.php
+++ protected/commands/I18NExportCommand.php
@@ -32,11 +32,24 @@ class I18NExportCommand extends CConsoleCommand
 		// Open target file to write
 		$fp = fopen("messages/_translation/chive.pot", "w");
 		
+		// Write headers
+		fputs($fp, 'msgid ""' . "\n");
+		fputs($fp, 'msgstr ""' . "\n");
+		fputs($fp, '"Project-Id-Version: chive 0.2.0\n"' . "\n");
+		fputs($fp, '"PO-Revision-Date:\n"' . "\n");
+		fputs($fp, '"Last-Translator: Fusonic\n"' . "\n");
+		fputs($fp, '"Language-Team: Fusonic\n"' . "\n");
+		fputs($fp, '"MIME-Version: 1.0\n"' . "\n");
+		fputs($fp, '"Content-Type: text/plain; charset=utf-8\n"' . "\n");
+		fputs($fp, '"Content-Transfer-Encoding: 8bit\n"' . "\n");
+		fputs($fp, "\n");
+		
 		// Write all strings
 		foreach(array_unique($strings) as $string)
 		{
-			fputs($fp, 'msgid "' . $string . '"' . "\n");
+			fputs($fp, 'msgid "' . addcslashes($string, '"') . '"' . "\n");
 			fputs($fp, 'msgstr ""' . "\n");
+			fputs($fp, "\n");
 		}
 		
 		fclose($fp);
