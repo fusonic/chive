4d72049550a12181eb858b803ed9b268c542f11c
David Roth <david.roth@fusonic.net>
2011-07-21 11:19:32 +0200

    added countries filter finished translations

--- protected/commands/I18NImportCommand.php
+++ protected/commands/I18NImportCommand.php
@@ -1,5 +1,6 @@
 <?php
 
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 class I18NImportCommand extends CConsoleCommand
 {
@@ -47,7 +48,6 @@ class I18NImportCommand extends CConsoleCommand
 				$stringKeys[] = (string)$entry;
 			}
 		}
-		
 		//
 		// This is a quick dirty hack because Gettext_PDO does can't handle escaped field delimiters.
 		// So i replace all espaced delimiters with -!!- and reverse it afterwards.
@@ -69,7 +69,7 @@ class I18NImportCommand extends CConsoleCommand
 		{
 			$key = str_replace('-!!-', '"', $key);
 			$value = str_replace('-!!-', '"', $value);
-			
+
 			$pos = array_search($key, $stringKeys);
 			
 			if($pos > -1)
@@ -102,27 +102,49 @@ class I18NImportCommand extends CConsoleCommand
 		}
 		
 		@mkdir('messages/' . strtolower($targetLang));
-		
+
+		$finishedTranslated = array(
+			"pt_br",
+			"en_gb",
+			"fr_fr",
+			"it_it",
+			"ru_ru",
+			"uk_uk",
+			"pl_pl",
+			"es_es");
+
 		// Translate
 		foreach($files as $file)
 		{
 			$xml = simplexml_load_file('messages/en/' . $file . '.xml');
 			$xmlNew = new SimpleXMLElement('<category id="' . $xml['id'] . '" />');
+
+			$dom = new DOMDocument("1.0", "utf-8");
+			$dom->formatOutput = true;
+
+			$category = $dom->createElement("category", null);
+			$category->setAttribute("id", $xml['id']);
+			$dom->appendChild($category);
 			
 			foreach($xml->entry as $entry)
 			{
 				if(isset($strings[(string)$entry]))
 				{
-					$entry2 = $xmlNew->addChild('entry', $strings[(string)$entry]);
-					$entry2->addAttribute('id', $entry['id']);
+					$child = $dom->createElement("entry", $strings[(string)$entry]);
+					$child->setAttribute("id", $entry['id']);
+					$category->appendChild($child);
 				}
 			}
-			
-			$dom = dom_import_simplexml($xmlNew)->ownerDocument;
+
+			/**
+			 * Reload into new DOM because otherwise formatOutput does not work (whyever ? ..)
+			 */
+			$content = $dom->saveXml();
+			$dom = new DOMDocument("1.0", "utf-8");
+			$dom->loadXML($content);
 			$dom->formatOutput = true;
-			
+
 			var_dump(file_put_contents('messages/' . strtolower($targetLang) . '/' . $file . '.xml', $dom->saveXML()));
 		}
 	}
-	
 }
