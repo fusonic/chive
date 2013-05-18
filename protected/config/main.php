be62c3dc8b11ce4c557654446cdd95042ea009d0
David Roth <david.roth@fusonic.net>
2013-05-18 14:27:07 +0200

    Chive 1.3

--- protected/config/main.php
+++ protected/config/main.php
@@ -232,7 +232,7 @@ return array(
 	// application-level parameters
 	'params' => array(
 		'iconPack' => 'fugue',
-		'version' => '1.2',
+		'version' => '1.3',
 	),
 
 	// There is no source language ...
