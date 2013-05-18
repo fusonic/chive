ca17fea6fea9364433ef52298a60cc533c91ec4b
David Roth <david.roth@fusonic.net>
2013-05-18 15:05:45 +0200

    Updated archiveExport to use git

--- archiveExport.sh
+++ archiveExport.sh
@@ -6,13 +6,14 @@ rm -rf /tmp/$VERSION/
 mkdir /tmp/$VERSION/
 mkdir /tmp/$VERSION/chive/
 echo exporting to /tmp/$VERSION/chive/
-bzr export /tmp/$VERSION/chive/
+git checkout-index -f -a --prefix=/tmp/$VERSION/chive/
 chmod 0777 -R /tmp/$VERSION/chive/assets
 chmod 0777 -R /tmp/$VERSION/chive/protected/runtime/
 
 cd /tmp/$VERSION/
 touch chive/index_changed.php
 cat chive/index.php | sed "s/'YII_DEBUG', true/'YII_DEBUG', false/" >> chive/index_changed.php
+rm chive/.gitignore
 rm chive/index.php
 rm chive/archiveExport.sh
 rm chive/protected/tests -rf
